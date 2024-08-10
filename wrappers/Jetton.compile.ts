6639fb932b443de5ca5bfeca84c67d255a00e236
wongyikcheung <wongyikcheung@hashkey.cloud>
2024-08-10 18:46:03 +0800

    update

--- wrappers/Jetton.compile.ts
+++ wrappers/Jetton.compile.ts
@@ -2,5 +2,5 @@ import { CompilerConfig } from '@ton/blueprint';
 
 export const compile: CompilerConfig = {
     lang: 'func',
-    targets: ['contracts/imports/jetton-minter.fc'],
+    targets: ['contracts/jetton-minter.fc'],
 };
